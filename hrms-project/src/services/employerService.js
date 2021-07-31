71913823f419674c71a27a0dcd5cba54ad74f76a
nurseda diker <54947744+nurseda-diker@users.noreply.github.com>
2021-07-31 15:16:37 +0300

    Delete hrms-project directory

--- hrms-project/src/services/employerService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-
-export default class EmployerService{
-    getEmployers(){
-        return axios.get("http://localhost:8080/api/employer/getall")
-    }
-}
--- hrms-project/src/services/jobSeekerService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default class JobSeekerService{
-    getJobSeekers(){
-        return axios.get("http://localhost:8080/api/jobSeeker/getall")
-    }
-}
